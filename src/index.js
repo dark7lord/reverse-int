6ae57a1927f89fd503da03e003de80bcb1509566
gopal <gopal@student.21-school.ru>
2023-01-09 15:42:26 +0300

    feat: finish exercize

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+const reverse = (n) => {
+    const result = n
+        .toString()
+        .split('')
+        .reverse()
+        .join('');
+    
+    return Number(result);
 }
+
+module.exports = reverse;
